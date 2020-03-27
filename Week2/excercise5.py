7e0385b0e71db3dfc62f280d99bede9c14c516b3
JohnnyPastrami <ryanleemcdonough@gmail.com>
2020-03-27 07:10:40 -0700

    finished week2

--- Week2/excercise5.py
+++ Week2/excercise5.py
@@ -10,3 +10,17 @@ Print both local AS number and the BGP peer IP address to the screen.
 https://github.com/ktbyers/pynet/tree/master/learning_python/lesson2
 '''
 
+with open('show_ip_bgp_summ.txt') as f:
+    bgp_sum = f.readlines()
+
+bgp_first = bgp_sum[1].strip()
+bgp_last = bgp_sum[-1].strip()
+
+bgp_first = bgp_first.split()
+bgp_first = bgp_first[-1]
+
+bgp_last = bgp_last.split()
+bgp_last = bgp_last[0]
+
+
+print(bgp_first, bgp_last)
