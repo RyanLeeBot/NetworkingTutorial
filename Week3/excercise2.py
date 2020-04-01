36a6775c13cbac4312a3cb7fd4d495692866b108
JohnnyPastrami <ryanleemcdonough@gmail.com>
2020-04-01 07:00:14 -0700

    ex2

--- Week3/excercise2.py
+++ Week3/excercise2.py
@@ -8,3 +8,24 @@ Using a conditional statement, also search for '10.220.88.30'. If this IP addres
 Keep track of whether you have found both the Default Gateway and the Arista3 switch. Once you have found both of these devices, 'break' out of the for loop.
 '''
 
+from pprint import pprint
+gateway, arista = (False, False)
+
+with open('show_arp.txt') as f:
+    show_arp = f.read()
+
+for line in show_arp.splitlines():
+    if 'Protocol' in line:
+        continue
+    fields = line.split()
+    ip_addr = fields[1]
+    mac_addr = fields[3] 
+    if ip_addr == '10.220.88.1':
+        print('Default gateway IP/Mac: {}{}'.format(ip_addr, mac_addr))
+        gateway = True
+    if ip_addr == '10.220.88.30':
+        print('Arista3 IP/Mac is {}{}'.format(ip_addr, mac_addr))
+        arista = True
+    if gateway and arista == True:
+        break
+
