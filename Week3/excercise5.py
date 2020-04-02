8d716d4c7803c49cb8d6b0b749b6f83554a9683c
RyanLeeBot <ryanleemcdonough@gmail.com>
2020-04-02 12:54:38 -0700

    finished week

--- Week3/excercise5.py
+++ Week3/excercise5.py
@@ -28,3 +28,35 @@ base_cmd_windows = 'ping -n 2'
 # Ternary operator
 base_cmd = base_cmd_windows if WINDOWS else base_cmd_linux
 '''
+from __future__ import unicode_literals, print_function
+import os
+
+# Toggle this to use Windows
+WINDOWS = True
+
+base_ip = '192.168.0.'
+base_cmd_linux = 'ping -c 2'
+base_cmd_windows = 'ping -n 2'
+# Ternary operator
+base_cmd = base_cmd_windows if WINDOWS else base_cmd_linux
+
+ip_list = []
+for last_octet in range(1, 255):
+    new_ip = base_ip + str(last_octet)
+    ip_list.append(new_ip)
+
+for i, ip_addr in enumerate(ip_list):
+    print("{} ---> {}".format(i, ip_addr))
+
+ip_list = ip_list[0:5]
+
+print(ip_list)
+
+print()
+print('-' * 80)
+for ip_addr in ip_list:
+    print("IP Addr: ", ip_addr)
+    return_code = os.system("{} {}".format(base_cmd, ip_addr))
+    print('-' * 80)
+print('-' * 80)
+print()
