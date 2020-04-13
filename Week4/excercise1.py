6ba4a360f9f67e5790f6c60f9d9323fdd4b66301
JohnnyPastrami <ryanleemcdonough@gmail.com>
2020-04-12 21:42:33 -0700

    ex1

--- Week4/excercise1.py
+++ Week4/excercise1.py
@@ -17,3 +17,39 @@ Using a for-loop, iterate over the dictionary and print out all of the dictionar
 Using a single for-loop, iterate over the dictionary and print out all of the dictionary keys and values.
 
 '''
+
+net_device = {
+    'ip_addr': '10.10.10.10',
+    'vendor': 'cisco',
+    'username': 'admin',
+    'password': 'password'
+}
+
+print()
+print(net_device['ip_addr'])
+print()
+
+if net_device['vendor'].lower() == 'cisco':
+    net_device['platform'] = 'ios'
+elif net_device['vendor'].lower() == 'juniper':
+    net_device['platform'] = 'junos'
+
+bgp_fields = {
+    'bgp_as': 42,
+    'peer_as': 100,
+    'peer_ip': '172.16.31.100'
+}
+
+net_device.update(bgp_fields)
+
+print('-' * 80)
+for key in net_device:
+    print("{:>15}".format(key))
+print('-' * 80)
+print()
+
+print('-' * 80)
+for key, value in net_device.items():
+    print("{key:>15} ---> {value:>15}".format(key=key, value=value))
+print('-' * 80)
+print()
